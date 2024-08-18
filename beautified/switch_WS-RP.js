77d35279a7f9b3b3901e6f8a92e377e40b205db4
Jean-Michel Bodart <jmbodart@skynet.be>
2024-08-18 16:40:09 +0200

    Update switch_WS-RP.js
    Remove intermediate variable: 'selected'
    Improve log messages

--- beautified/switch_WS-RP.js
+++ beautified/switch_WS-RP.js
@@ -3,22 +3,23 @@
   locs = ["REPOSITORY", "WORKSPACE"],
   ws = [3,4].map(n => iDoc.getElementById("sasLSAF--sasLSAF_appContainer_lfn_"+n)),
   icn = [3,4].map(n => iDoc.getElementById("sasLSAF--sasLSAF_appContainer_lfn_"+n+"_icn"));
-  const selected = ws.map(el => (el.className === "sasLeftNavItemSelected")),
-  selIdx = selected.indexOf(true),
+  // const selected = ws.map(el => (el.className === "sasLeftNavItemSelected")),
+  selIdx = ws.map(el => (el.className === "sasLeftNavItemSelected")).indexOf(true),
   qs = locs.map(loc => iDoc.getElementById("HLS_LSAF_" + loc + "--navLinkInput-inner"));
-  console.log('locs:', locs,'\nws:', ws, '\nicn:', icn, '\nselected:', selected, '\nselIdx:', selIdx);
+  // console.log('locs:', locs,'\nws:', ws, '\nicn:', icn, '\nselected:', selected, '\nselIdx:', selIdx);
   let curr = "";
   if (selIdx > -1) {
     curr = qs[selIdx].value;
     console.log('Currently in', locs[selIdx], 'path:', curr);
     console.log('otherLoc:', locs[1-selIdx], 'path:',  qs[1-selIdx]?.value);
+    console.log('Switching to '+locs[1-selIdx]+' path: '+curr+' ...');
     icn[1-selIdx].click();
     qs[1-selIdx].value = curr ;
     qs[1-selIdx].dispatchEvent(keyboardEvent = new KeyboardEvent("keydown", 
       { code: "Enter", key: "Enter", charCode: 13, keyCode: 13, view: window, bubbles: !0 }));
     setTimeout((() => {
       const e = iDoc.querySelectorAll(`.sapUiTreeNodeSelected[aria-selected="true"][id*="${locs[1-selIdx]}"]`);
-      console.log("sel", e);
+      // console.log("sel", e);
       if (e[0]) e[0].scrollIntoViewIfNeeded();
     }), 2e3);
   }
